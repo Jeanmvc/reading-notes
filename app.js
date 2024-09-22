63fa5d3ef40636c48df563a059870510cf9fcdb3
jeanmarcovilca@gmail.com <jeanmarcovilca@gmail.com>
2024-09-22 18:25:25 -0500

    se modifica el color de fondo de mi pagina index

---
+++ app.js
@@ -0,0 +1,11 @@
+const nombreUsuario = prompt("Digite su nombre");
+//const edadUsuario = prompt("Digite su edad");
+const colorFondo = prompt("Escriba el color de fondo");
+const correoUsuario = prompt("Digite su Correo")
+const saludoUsuario =prompt("Digite si es de Mañana, tarde o noche")
+
+//alert(nombreUsuario)
+//alert(edadUsuario)
+//alert(colorFondo)
+
+
